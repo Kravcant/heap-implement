927f807e862107e16ba287883b1b2d313beaa815
Ahmed Ihsan <Ihsan.Ahmed@student.greenriver.edu>
2026-05-29 16:29:23 -0700

    Added basic testing for peak and size methods for heap class

--- src/HeapTest.java
+++ src/HeapTest.java
@@ -46,4 +46,94 @@ public class HeapTest {
         heap.add(3);
         assertEquals(3, Heap.heap.size());
     }
+
+    // peak methods
+
+    @Test
+    public void testPeakReturnsMinElement() {
+        heap.add(10);
+        heap.add(3);
+        heap.add(7);
+        assertEquals(3, Heap.peak());
+    }
+
+    @Test
+    public void testPeakOnSingleElement() {
+        heap.add(42);
+        assertEquals(42, Heap.peak());
+    }
+
+    @Test
+    public void testPeakOnEmptyHeapReturnsNegativeOne() {
+        assertEquals(-1, Heap.peak());
+    }
+
+    @Test
+    public void testPeakDoesNotRemoveElement() {
+        heap.add(5);
+        heap.add(1);
+        Heap.peak();
+        assertEquals(2, Heap.getSize());
+    }
+
+    @Test
+    public void testPeakAfterAddingInOrder() {
+        heap.add(1);
+        heap.add(2);
+        heap.add(3);
+        assertEquals(1, Heap.peak());
+    }
+
+    @Test
+    public void testPeakAfterAddingInReverseOrder() {
+        heap.add(9);
+        heap.add(6);
+        heap.add(3);
+        assertEquals(3, Heap.peak());
+    }
+
+    @Test
+    public void testPeakWithDuplicates() {
+        heap.add(2);
+        heap.add(2);
+        heap.add(2);
+        assertEquals(2, Heap.peak());
+    }
+
+    // getSize methods
+
+    @Test
+    public void testSizeOnEmptyHeap() {
+        assertEquals(0, Heap.getSize());
+    }
+
+    @Test
+    public void testSizeAfterOneAdd() {
+        heap.add(1);
+        assertEquals(1, Heap.getSize());
+    }
+
+    @Test
+    public void testSizeAfterMultipleAdds() {
+        heap.add(1);
+        heap.add(2);
+        heap.add(3);
+        assertEquals(3, Heap.getSize());
+    }
+
+    @Test
+    public void testSizeWithDuplicateElements() {
+        heap.add(5);
+        heap.add(5);
+        heap.add(5);
+        assertEquals(3, Heap.getSize());
+    }
+
+    @Test
+    public void testSizeIsNotAffectedByPeak() {
+        heap.add(10);
+        heap.add(20);
+        Heap.peak();
+        assertEquals(2, Heap.getSize());
+    }
 }
