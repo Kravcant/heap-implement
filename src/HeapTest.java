6945d1f876256cf54369d3d6816bf8fe484148ed
anthonykravchishin <tonykravc53@gmail.com>
2026-05-29 16:08:25 -0700

    Implemented tests for add method

--- src/HeapTest.java
+++ src/HeapTest.java
@@ -3,4 +3,47 @@ import org.junit.Test;
 
 public class HeapTest {
 
+    private Heap heap = new Heap();
+
+    // add methods
+
+    @Test
+    public void testAddSingleElement() {
+        heap.add(5);
+        assertEquals(1, Heap.heap.size());
+        assertEquals(5, (int) Heap.heap.get(0));
+    }
+
+    @Test
+    public void testAddMaintainsMinHeapOrder() {
+        heap.add(10);
+        heap.add(3);
+        heap.add(7);
+        assertEquals(3, (int) Heap.heap.get(0));
+    }
+
+    @Test
+    public void testAddAlreadyOrdered() {
+        heap.add(1);
+        heap.add(2);
+        heap.add(3);
+        assertEquals(1, (int) Heap.heap.get(0));
+    }
+
+    @Test
+    public void testAddDuplicateElements() {
+        heap.add(4);
+        heap.add(4);
+        heap.add(4);
+        assertEquals(3, Heap.heap.size());
+        assertEquals(4, (int) Heap.heap.get(0));
+    }
+
+    @Test
+    public void testAddIncreasesSize() {
+        heap.add(1);
+        heap.add(2);
+        heap.add(3);
+        assertEquals(3, Heap.heap.size());
+    }
 }
